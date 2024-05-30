c84220654d8f586a53f007a30df2f9d72a47c10e
Nakshatra bansal <75303956+naksh2004@users.noreply.github.com>
2024-05-30 20:00:44 +0530

    Update wsgi.py

--- excel_upload_project/wsgi.py
+++ excel_upload_project/wsgi.py
@@ -11,14 +11,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from django.core.servers.basehttp import get_internal_wsgi_application
+from django.core.management.commands.runserver import Command
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "excel_upload_project.settings")
 
 application = get_wsgi_application()
-
-# Add below lines to start the server with specified configurations
-from django.core.servers.basehttp import get_internal_wsgi_application
-from django.core.management.commands.runserver import Command
 
 Command.default_port = os.environ.get('PORT', '8000')
 server = get_internal_wsgi_application()
