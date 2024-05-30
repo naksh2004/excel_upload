0d551f025dbe3650c180b3c48d833a6e8cd28228
Nakshatra bansal <75303956+naksh2004@users.noreply.github.com>
2024-05-30 19:59:22 +0530

    Update wsgi.py

--- excel_upload_project/wsgi.py
+++ excel_upload_project/wsgi.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "excel_upload_project.settings")
 
 application = get_wsgi_application()
 
