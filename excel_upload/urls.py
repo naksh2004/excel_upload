067f620231c9d5d726b4a79453fdf569b6735401
Nakshatra bansal <75303956+naksh2004@users.noreply.github.com>
2024-05-31 11:37:32 +0530

    Update urls.py

--- excel_upload/urls.py
+++ excel_upload/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.homepage, name='homepage'), 
     path('upload/', views.upload_file, name='upload_file'),
     path('generate_summary_report/', views.generate_summary_report, name='generate_summary_report'),  # Add this line for test upload
 ]
